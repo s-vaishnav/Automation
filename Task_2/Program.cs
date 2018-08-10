33c0637234650a42b66bb4dc282bd75db6cd03e2
sri-vaishnav <vaishnavsrirama@gmail.com>
2018-08-10 14:32:44 +0530

    Changes made to files

---
+++ Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WorkingDirectory = @"C:\Users\User\source\repos\Task_1_Copy\Task_1_Copy\bin\Debug";
+            process.Start();
+            process.StandardInput.WriteLine("Task_1_Copy.exe");
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
