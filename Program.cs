bbaa254d558c0eec29e5a0a33f0ca40fb916a3fc
sri-vaishnav <vaishnavsrirama@gmail.com>
2018-08-10 14:11:20 +0530

    Added code to repos

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Copy
+{
+    class File_Copy
+    {
+        static void Main(string[] args)
+        {
+            CloneDirectory(@"E:\dir", @"G:\files");
+        }
+
+        private static void CloneDirectory(string root, string dest)
+        {
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(dest, dirName)))
+                {
+                    Directory.CreateDirectory(Path.Combine(dest, dirName));
+                }
+                CloneDirectory(directory, Path.Combine(dest, dirName));
+            }
+
+            foreach (var file in Directory.GetFiles(root))
+            {
+                File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+            }
+        }
+
+    }
+}
