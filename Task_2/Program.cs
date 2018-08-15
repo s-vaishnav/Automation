ccdd0f471d25e2ed61588ace8182204c67ced54c
sri-vaishnav <vaishnavsrirama@gmail.com>
2018-08-15 15:42:25 +0530

    Tast 2 Completed

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp2
 {
@@ -18,7 +19,17 @@ namespace ConsoleApp2
             process.StartInfo.RedirectStandardInput = true;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.UseShellExecute = false;
-            process.StartInfo.WorkingDirectory = @"C:\Users\User\source\repos\Task_1_Copy\Task_1_Copy\bin\Debug";
+
+
+            Console.WriteLine("Enter the Directory where operation sholud be performed(eg:- c or d ..");
+            // string C = Console.ReadLine();
+            // String str;
+            // String query = "% SystemDrive %";
+            // str = Environment.ExpandEnvironmentVariables(query);
+            // string path1 =  C + ":";
+            // string path2 = "\\Users\\User\\source\repos\\Task_1_Copy\\Task_1_Copy\bin\\Debug";
+            var actualPath = Environment.ExpandEnvironmentVariables(@"%SystemDrive%\Users\User\source\repos\Task_1_Copy\Task_1_Copy\bin\Debug");
+            process.StartInfo.WorkingDirectory = actualPath;
             process.Start();
             process.StandardInput.WriteLine("Task_1_Copy.exe");
             process.StandardInput.Flush();
